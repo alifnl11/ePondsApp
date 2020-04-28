7181b6b24376793db04ce6a9023746fc08584f5b
M Alif Nurrofli L <zerpprogaming@gmail.com>
2020-04-28 23:04:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCDKMW3yWeu3oG7g5Oq4jhDQZ7Qpj6dvHg",
-    authDomain: "eponds-7adcf.firebaseapp.com",
-    databaseURL: "https://eponds-7adcf.firebaseio.com",
-    projectId: "eponds-7adcf",
-    storageBucket: "eponds-7adcf.appspot.com",
-    messagingSenderId: "787184521925",
-    appId: "1:787184521925:web:3604982cbfd98606bb56ac",
-    measurementId: "G-DWJTLBS6X0"
+    apiKey:,
+    authDomain:,
+    databaseURL:
+    projectId:,
+    storageBucket:,
+    messagingSenderId:,
+    appId:,
+    measurementId: 
   };
 
 
